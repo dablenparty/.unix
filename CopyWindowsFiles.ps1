557b80a8fdba75364d2534f60f3e405e12d42ac3
dablenparty <hdavenport8902@gmail.com>
2024-12-07 21:54:20 -0500

    Add extra output to CopyWindowsFiles.ps1

--- CopyWindowsFiles.ps1
+++ CopyWindowsFiles.ps1
@@ -1,8 +1,10 @@
+Write-Output "Copying Alacritty config"
 $AlacrittyConfigFolder = "$env:APPDATA\alacritty"
 if (-Not (Test-Path -PathType Container -Path "$AlacrittyConfigFolder")) {
     New-Item -ItemType Directory -Path "$AlacrittyConfigFolder"
 }
 Copy-Item -Path ".\.alacritty.windows.toml" -Destination "$AlacrittyConfigFolder\alacritty.toml"
+Write-Output "Copying OMP config"
 $OmpConfig = ".\zen.omp.toml"
 Copy-Item -Path $OmpConfig -Destination "$env:POSH_THEMES_PATH\zen.omp.toml"
 Write-Output "Make sure to source $env:POSH_THEMES_PATH\zen.omp.toml as the OMP config in your PowerShell Profile"
