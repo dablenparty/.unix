12a6a75f73006d43bca92c0e5ee663d08e69d752
dablenparty <anoptimisticcouch@gmail.com>
2025-02-01 11:47:29 -0500

    Add missing newline in good_morning.sh

--- scripts/good_morning.sh
+++ scripts/good_morning.sh
@@ -1,5 +1,4 @@
 rustup toolchain update nightly
-brew update && brew upgrade
 yay -Syu
 
-printf 'Don''t forget to update \033[0;36mHyprland\033[0m!'
+printf 'Don''t forget to update \033[0;36mHyprland\033[0m!\n'
