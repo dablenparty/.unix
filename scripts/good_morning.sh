8767e92c6a962ba3cd818734b52960d169029fb1
dablenparty <anoptimisticcouch@gmail.com>
2025-03-19 21:36:19 -0400

    refactor(good_morning.sh)!: remove all user interaction
    Since I created the PKGBUILD, Hyprland will be updated with paru. As for
    rustup, I realize that updating the stable toolchain should probably be
    a conscious decision.

--- scripts/good_morning.sh
+++ scripts/good_morning.sh
@@ -1,33 +1,7 @@
 #!/usr/bin/env bash
 
-wait_for_y_key() {
-  if [[ -n "$1" ]]; then
-    prompt="$1"
-  else
-    prompt="Would you like to continue? [Y\n]"
-  fi
-  read -p "$prompt" -r -s -n 1 key
-  printf '\n'
-
-  case $key in
-  y | Y | "") return 0 ;;
-  n | N)
-    return 1
-    ;;
-  *)
-    return 1
-    ;;
-  esac
-}
+set -e
 
 paru
-
-if wait_for_y_key "Update rustup? (Nightly always updates) [Y\n]"; then
-  rustup update
-else
-  rustup toolchain update nightly
-fi
-
-if wait_for_y_key "Update Hyprland? [Y\n]"; then
-  bash "$(readlink "$HOME/update_hyprland.sh")"
-fi
+rustup self update
+rustup toolchain update nightly
