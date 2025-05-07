735be2074654dac619848b7786e9e5bd0e929307
dablenparty <anoptimisticcouch@gmail.com>
2025-05-07 14:15:25 -0400

    feat(setup_on_arch.sh): first full implementation

--- setup_on_arch.sh
+++ setup_on_arch.sh
@@ -8,6 +8,9 @@ echo "    sudo pacman -Syu"
 read -rp "Continue? [Y\n]" -n 1 key
 printf "\n"
 
+# save the original dir for undoing cd commands
+ORIG_DIR="$PWD"
+
 case $key in
 y | Y | "")
   echo "Beginning installation..."
@@ -17,6 +20,13 @@ y | Y | "")
   exit 0
   ;;
 esac
+
+echo "installing CachyOS repos"
+cachy_tar_path=/tmp/cachyos-repo.tar.xz
+cachy_extract_path=/tmp/cachyos-repo
+curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o "$cachy_tar_path"
+tar -xvf "$cachy_tar_path" -C /tmp
+sudo "$cachy_extract_path"/cachyos-repo.sh
 
 echo "installing dependencies"
 sudo pacman --overwrite "*" --needed --noconfirm -S \
@@ -54,14 +64,78 @@ read -rep "git email: " git_email
 printf "\n"
 git config --global user.email "$git_email"
 
-echo "installing boxunbox"
+echo "temp installing boxunbox"
 boxunbox_path="$HOME/Documents/repos/boxunbox"
 # make parent dir(s)
 mkdir -vp "${boxunbox_path%/*}"
 git clone https://github.com/dablenparty/boxunbox.git "$boxunbox_path"
-cargo install --path "$boxunbox_path"
+cargo build --release
 
-# TODO: download and install dotfiles
-# TODO: download and install paru
-# TODO: install cachy repos
-# TODO: install neovim dependencies
+force_unbox() {
+  if (($# < 1)); then
+    echo "usage: $0 <package_dir> [package_dirs...]" 1>&2
+    return 1
+  fi
+  unbox_bin="$boxunbox_path/target/release/unbox"
+  "$unbox_bin" --force "$@"
+}
+
+echo "installing AUR helper: paru"
+paru_path="$HOME/aur/paru"
+sudo pacman --noconfirm --needed -S base-devel
+git clone https://aur.archlinux.org/paru.git "$paru_path"
+cd "$paru_path" || exit 1
+# paru is a Rust project and I don't use any crazy custom RUSTFLAGS,
+# so it's OK to do this before unboxing the makepkg config in my dotfiles.
+# I actually like having paru be different.
+makepkg -si
+cd "$ORIG_DIR" || exit 1
+paru --gendb
+
+echo "installing dotfiles"
+dotfiles_path="$HOME/dotfiles"
+modules_to_unbox=(home lazygit makepkg neovim pacman paru yazi)
+# TODO: once boxunbox is in the AUR, install it here
+paru --needed --noconfirm -S \
+  bat \
+  eza \
+  fd \
+  fnm \
+  fzf \
+  gcc \
+  git \
+  jenv \
+  lazygit \
+  make \
+  oh-my-posh-bin \
+  neovim \
+  ripgrep \
+  unzip \
+  zoxide \
+  zsh
+git clone https://github.com/dablenparty/.unix.git "$dotfiles_path"
+cd "$dotfiles_path" || exit 1
+git submodule update --init --remote --rebase neovim
+cd "$ORIG_DIR" || exit 1
+force_unbox "${modules_to_unbox[@]}"
+
+paru --needed --noconfirm -S oh-my-posh-bin
+
+echo "installing gaming dependencies"
+paru --noconfirm --needed -S wine \
+  wine-gecko \
+  wine-mono \
+  pipewire-pulse \
+  lib32-pipewire \
+  lib32-libpulse \
+  lib32-sdl2 \
+  xdg-desktop-portal-kde
+
+# steam must be installed after all the Wine stuff
+paru --noconfirm --needed --rebuild=all -S steam
+
+echo "installing extras"
+paru --needed --noconfirm --asexplicit -S \
+  obsidian \
+  syncthingtray-qt6 \
+  syncthing
